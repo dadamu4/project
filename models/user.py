7c262299c36f855e4880f2e9fab12112a77befd4
cssi <cssi@cssis-MacBook-Pro.local>
2019-07-24 10:45:46 -0400

    So she wont get mad at me

--- models/user.py
+++ models/user.py
@@ -4,17 +4,20 @@ from google.appengine.api import users
 import datetime
 
 class Comment(ndb.Model):
-    date = datetime.datetime.now().strftime("%x")
+    # date = ndb.DatetimeProperty(auto_add_now = True)
     title = ndb.StringProperty(required = True)
     comment = ndb.StringProperty(required = True)
 
-class User(ndb.Model):
-    email = ndb.StringProperty(required = True)
-    firstName = ndb.StringProperty(required = False)
-    lastName = ndb.StringProperty(required = False)
+class Financials(ndb.Model):
     moneySpent = ndb.StringProperty(required = False)
     weeklyAllowance = ndb.StringProperty(required = False)
     savings = ndb.StringProperty(required = False)
     utilities = ndb.StringProperty(required = False)
     fun = ndb.StringProperty(required = False)
+
+class User(ndb.Model):
+    email = ndb.StringProperty(required = True)
+    firstName = ndb.StringProperty(required = False)
+    lastName = ndb.StringProperty(required = False)
+    money = ndb.KeyProperty(Financials, repeated=True)
     comments = ndb.KeyProperty(Comment, repeated=True)
