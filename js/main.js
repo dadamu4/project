310c3265e94cf84611430a7ea4b3b237820b4ff6
cssi <cssi@cssis-MacBook-Pro.local>
2019-07-25 17:22:34 -0400

    Correct

--- js/main.js
+++ js/main.js
@@ -17,3 +17,15 @@ subtracting.forEach(function(elm, i){
     countdown[i].innerHTML --
   });
 });
+
+// let hoverDivs = document.querySelectorAll(".column");
+// let colincol = document.querySelectorAll(".columnInCol");
+//
+// hoverDivs.forEach(function(elm, i){
+//   elm.addEventListener("mouseover", function(){
+//     colincol[i].classList.remove("makeMeVisible")
+//   });
+//   elm.addEventListener("mouseout", function(){
+//     colincol[i].classList.add("makeMeVisible")
+//   });
+// });
